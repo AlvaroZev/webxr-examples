c32dd4b0a68ef653817de780f829a5349fe7d23c
AlvaroZev <azevallos1202@gmail.com>
2022-08-26 10:28:15 -0500

    feat: many squares

--- pages/three/FirstGear/index.tsx
+++ pages/three/FirstGear/index.tsx
@@ -8,6 +8,7 @@ import {
   Color,
   DirectionalLight,
   DoubleSide,
+  Euler,
   GridHelper,
   HemisphereLight,
   Mesh,
@@ -94,15 +95,57 @@ export default class App extends Component {
     // highlight.position.set(0.1, 0.1, 1);
     // this.scene.add(highlight);
 
+    //aqui estaba
+
+    // const material = [];
+    const vertices = geometry.attributes.position.array;
+
+    const length = vertices.length;
+
+    const position = geometry.attributes.position;
+    const vector = new Vector3();
+    const planerotation = new Euler();
+    // console.log(position);
+
+    // material.push(new MeshBasicMaterial({ color: 0xffffff }));
+    // material.push(new MeshBasicMaterial({ color: 0xff0000 }));
+    // material.push(new MeshBasicMaterial({ color: "yellow" }));
+    // material.push(new MeshBasicMaterial({ color: "blue" }));
+    // material.push(new MeshBasicMaterial({ color: "navy" }));
+
+    const sphere = new Mesh(geometry, material);
+
+    
+    this.scene.add(sphere);
+
+    const controls = new OrbitControls(this.camera, this.renderer.domElement);
+
+    //crear planos
     var planes = [];
 
     var spriteResponse = [];
-    spriteResponse[0] = {
-      ID: 1,
-      x: 0.5831503868103027,
-      y: 0.80901700258255,
-      z: 0.07366902381181717,
-    };
+
+    for (let i = 0, l = position.count; i < l; i++) {
+      vector.fromBufferAttribute(position, i);
+      vector.applyMatrix4(sphere.matrixWorld);
+      //console.log(vector);
+      spriteResponse[i]={
+        ID: i+1,
+        x:vector.x,
+        y: vector.y,
+        z: vector.z,
+        Rx: i*360/50,
+        Ry: i*360/25,
+        Rz: i*360/50,
+
+      };
+    }
+    //spriteResponse[0] = {
+    //  ID: 1,
+    //  x: 0.5831503868103027,
+    //  y: 0.80901700258255,
+    //  z: 0.07366902381181717,
+    //};
     // // spriteResponse[1] = { ID: 2, x: 0, y: 0.1, z: 0 };
     // // spriteResponse[2] = { ID: 3, x: 0, y: 0.5, z: 0 };
     // // spriteResponse[3] = { ID: 4, x: 0.5, y: 0, z: 0 };
@@ -114,7 +157,16 @@ export default class App extends Component {
         side: DoubleSide,
       });
       material_plane.needsUpdate = true;
-      const geometry_plane = new PlaneGeometry(0.1, 0.1);
+      var geometry_plane = new PlaneGeometry(0.01, 0.01);
+      if (i<90) 
+      {
+        console.log("ola")
+        var geometry_plane = new PlaneGeometry(0.1,0.1);
+      }
+      else 
+      {
+        console.log("olachao")
+      }
       const plane = new Mesh(geometry_plane, material_plane);
 
       plane.position.set(
@@ -122,6 +174,11 @@ export default class App extends Component {
         spriteResponse[i].y,
         spriteResponse[i].z
       );
+
+      //planerotation.setFromRotationMatrix(m: sphere.matrixWorld);
+      //plane.setRotationFromEuler(euler: planerotation );
+      
+      console.log(plane.rotation);//console.log(plane.quaternion)
 
       this.scene.add(plane);
 
@@ -133,34 +190,6 @@ export default class App extends Component {
       // plane.lookAt(vector);
       // plane.userData = { URL: "http://stackoverflow.com" };
     }
-
-    // const material = [];
-    const vertices = geometry.attributes.position.array;
-
-    const length = vertices.length;
-
-    const position = geometry.attributes.position;
-    const vector = new Vector3();
-
-    // console.log(position);
-
-    // material.push(new MeshBasicMaterial({ color: 0xffffff }));
-    // material.push(new MeshBasicMaterial({ color: 0xff0000 }));
-    // material.push(new MeshBasicMaterial({ color: "yellow" }));
-    // material.push(new MeshBasicMaterial({ color: "blue" }));
-    // material.push(new MeshBasicMaterial({ color: "navy" }));
-
-    const sphere = new Mesh(geometry, material);
-
-    for (let i = 0, l = position.count; i < l; i++) {
-      vector.fromBufferAttribute(position, i);
-      vector.applyMatrix4(sphere.matrixWorld);
-      console.log(vector);
-    }
-
-    this.scene.add(sphere);
-
-    const controls = new OrbitControls(this.camera, this.renderer.domElement);
 
     window.addEventListener("resize", this.resize.bind(this));
   };
